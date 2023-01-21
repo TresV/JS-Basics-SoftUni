ea7cdd1de3729307fbaf8eb921570bd634e26a19
Victor Varbanov <9961253+TresV@users.noreply.github.com>
2023-01-21 20:33:20 +0200

    updated solution

--- PB-online-exam-6-7-april-2019/06-cinema-tickets.js
+++ PB-online-exam-6-7-april-2019/06-cinema-tickets.js
@@ -1,96 +1,43 @@
 function cinemaTickets(input) {
-    let student = 0;
-    let standard = 0;
-    let kid = 0;
-  
     let index = 0;
-  
-    while (true) {
-  
-      let command = input[index];
-      if (command === 'Finish') {
-        break;
-      }
-  
-      index++;
-      let freeSeats = Number(input[index]);
-      index++;
-  
-      let ticketsCurrentMovie = 0;
-  
-      let ticket = input[index];
-      while (ticket !== 'Finish' && ticketsCurrentMovie < freeSeats) {
-        switch (ticket) {
-          case "student": student++; break;
-          case "standard": standard++; break;
-          case "kid": kid++; break;
+    let command = input[index++];
+    let studentTicketCounter = 0;
+    let standardTicketCounter = 0;
+    let kidTicketCounter = 0;
+    let movie;
+
+    while (command !== "Finish") {
+        movie = command;
+        let emptySeats = Number(input[index++]);
+        let newCommand = input[index++];
+        let soldTicket = 0;
+        while (newCommand !== "End") {
+            let ticketType = newCommand;
+            soldTicket++;
+            switch (ticketType) {
+                case "student":studentTicketCounter++;break;
+                case "standard":standardTicketCounter++;break;
+                case "kid":kidTicketCounter++;break;
+            }
+            if (soldTicket === emptySeats) {
+                break;
+            }
+            newCommand = input[index++];
         }
-  
-        ticketsCurrentMovie++;
-        index++;
-        ticket = input[index];
-  
-        if (ticket === 'End') {
-          index++;
-          break;
-        }
-      }
-  
-      console.log(`${command} - ${((ticketsCurrentMovie / freeSeats) * 100).toFixed(2)}% full.`);
-  
+        let fullness = (soldTicket / emptySeats) * 100;
+        console.log(`${movie} - ${fullness.toFixed(2)}% full.`);
+        command = input[index++];
     }
-  
-    let ticketsBought = student + standard + kid;
-    console.log(`Total tickets: ${ticketsBought}`);
-    console.log(`${((student / ticketsBought) * 100).toFixed(2)}% student tickets.`);
-    console.log(`${((standard / ticketsBought) * 100).toFixed(2)}% standard tickets.`);
-    console.log(`${((kid / ticketsBought) * 100).toFixed(2)}% kids tickets.`);
-  }
-
-  cinemaTickets(["Taxi",
-  "10",
-  "standard",
-  "kid",
-  "student",
-  "student",
-  "standard",
-  "standard",
-  "End",
-  "Scary Movie",
-  "6",
-  "student",
-  "student",
-  "student",
-  "student",
-  "student",
-  "student",
-  "Finish"])
-
-  cinemaTickets(["The Matrix",
-  "20",
-  "student",
-  "standard",
-  "kid",
-  "kid",
-  "student",
-  "student",
-  "standard",
-  "student",
-  "End",
-  "The Green Mile",
-  "17",
-  "student",
-  "standard",
-  "standard",
-  "student",
-  "standard",
-  "student",
-  "End",
-  "Amadeus",
-  "3",
-  "standard",
-  "standard",
-  "standard",
-  "Finish"])
-  
-  
+    let totalTickets =studentTicketCounter + standardTicketCounter + kidTicketCounter;
+    let percentStudentTickets = (studentTicketCounter / totalTickets) * 100;
+    let percentStandardTickets = (standardTicketCounter / totalTickets) * 100;
+    let percentKidTickets = (kidTicketCounter / totalTickets) * 100;
+    
+    console.log(`Total tickets: ${totalTickets}`);
+    console.log(`${percentStudentTickets.toFixed(2)}% student tickets.`);
+    console.log(`${percentStandardTickets.toFixed(2)}% standard tickets.`);
+    console.log(`${percentKidTickets.toFixed(2)}% kids tickets.`);
+}
+cinemaTickets(["The Matrix","20","student","standard","kid","kid","student","student","standard","student","End",
+"The Green Mile","17","student","standard","standard","student","standard","student","End",
+"Amadeus","3","standard","standard","standard","Finish",]);
